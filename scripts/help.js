5191186733b89c92fd33422591f9ea7da66c507a
realrhys <rhys@relatid.com>
2018-08-16 11:26:21 -0400

    Added the feature to toggle admin features in any environment.

---
+++ scripts/help.js
@@ -0,0 +1,27 @@
+const request = require('request');
+const Client = require('kubernetes-client').Client;
+const config = require('kubernetes-client').config;
+const client = new Client({ config: config.getInCluster() });
+client.loadSpec();
+
+module.exports = function(robot) {
+
+    const help =
+        'Commands\n' +
+        '=====\n.' +
+        '\n' +
+
+        'vbot list (server | ui) versions\n' +
+        'vbot promote (dev | uat) (server | ui)\n' +
+        'vbot revert (uat | prod) (server | ui) to version (DOCKER_VERSION_NUMBER)\n' + 
+        'vbot turn (dev | uat | perf) admin (on | off)\n';
+        
+
+    robot.respond(/help/i, function(res) {
+        (async () => {
+            const deploy = await client.apis.apps.v1.namespaces('vbot').deployments('vbot-deployment').get()
+            const message = help + '\n\nCurrent version of the vbot: ' + deploy.body.spec.template.spec.containers[0].image;
+            res.send(message);
+        })();
+    });
+}
---
+++ scripts/ingress.js
@@ -0,0 +1,63 @@
+const ingressOn = require('./kube/server/ingress-on.json');
+const ingressOff = require('./kube/server/ingress-off.json');
+const request = require('request');
+const Client = require('kubernetes-client').Client;
+const config = require('kubernetes-client').config;
+const client = new Client({ config: config.getInCluster() });
+client.loadSpec();
+
+module.exports = function(robot) {
+
+    const developers = process.env.DEVELOPERS.split(",");
+    const superusers = process.env.SUPERUSERS.split(",");
+
+    robot.respond(/turn (.*) admin (.*)/i, function(res) {
+        const name          =  res.envelope.user.name;
+        const environment   = res.match[1];
+        const toggle        = res.match[2];
+
+        if(!developers.includes(name)) {
+            res.send("Sorry " + name + ". I'm afraid I can't let you do that.");
+        } else if (environment === 'prod' && !superusers.includes(name)) {
+            res.send("Sorry " + name + ". I'm afraid I can't let you do that.");
+        } else if (environment !== 'dev' && environment !== 'uat' && environment !== 'perf'&& environment !== 'prod') {
+            const error = 
+                environment + " is NOT a known environment.\n" +
+                "Supported ENVs: \n" +
+                "================ \n" +
+                "  1.dev\n" +
+                "  2. uat\n" +
+                "  3. perf\n" +
+                "  4. prod\n";
+            res.send(error);
+        } else if (toggle !== 'on' && toggle !== 'off') {
+            const error = 
+                toggle + " is NOT a known\n" +
+                "Supported: \n" +
+                "================ \n" +
+                "  1. on\n" +
+                "  2. off\n";
+
+            res.send(error);
+        } else {
+            var ingress = {};
+            if (toggle === 'on') {
+                ingress = ingressOn;
+            } else {
+                ingress = ingressOff;
+            }
+            ingress.metadata.namespace = 'venue-' + environment;
+            if (environment === 'prod') {
+                //change this once the DNS us rerouted.
+                // ingress.spec.rules[0].host = 'venue.volentix.io';
+                ingress.spec.rules[0].host = 'venue-' + environment +'.volentix.io';
+            } else {
+                ingress.spec.rules[0].host = 'venue-' + environment +'.volentix.io';
+            }
+            (async () => {                
+                const createperf = await client.apis.extensions.v1beta1.namespaces('venue-' + environment).ingresses('venue-django-ing').put({ body: ingress});
+                res.send("UI: Please check that the update has been successful.");
+            })();
+        }
+    });
+}
